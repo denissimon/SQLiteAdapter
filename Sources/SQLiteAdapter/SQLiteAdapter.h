bd59c3999c88af24686a79a7a6bdbce12449a76a
Denis <denis.v.simon@gmail.com>
2024-01-23 13:41:14 +0500

    Added Carthage support

---
+++ Sources/SQLiteAdapter/SQLiteAdapter.h
@@ -0,0 +1,18 @@
+//
+//  SQLiteAdapter.h
+//  https://github.com/denissimon/SQLiteAdapter
+//
+//  Created by Denis Simon on 21/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SQLiteAdapter.
+FOUNDATION_EXPORT double SQLiteAdapterVersionNumber;
+
+//! Project version string for SQLiteAdapter.
+FOUNDATION_EXPORT const unsigned char SQLiteAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLiteAdapter/PublicHeader.h>
+
+
